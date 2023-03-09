a7f21aaf5afb9d5660007c74eb0dc9c17196bf97
Joe Porembski <jporembski@gmail.com>
2023-03-08 23:37:54 -0500

    Stash of the night's work

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,18 @@
 Copyright 2023 Joe Porembski
 SPDX-License-Identifier: BSD-3-Clause
 ------------------------------------------------------------------------------*/
+#include "connectivity/wifi-connection.hpp"
 #include "generated/configuration.hpp"
 #include "sensors/constants.hpp"
 #include "sensors/dht.hpp"
 
 #include <boards/pico_w.h>
 #include <hardware/gpio.h>
+#include <pico/stdio.h>
 #include <pico/stdlib.h>
 
 #include <cstdint>
+#include <cstdio>
 
 #ifdef RASPBERRYPI_PICO_W
 inline constexpr uint8_t LED_PIN = UINT8_MAX;
@@ -23,6 +26,11 @@ inline constexpr uint8_t DHT_DATA_PIN = 22;
 
 int main(int argc, char** argv)
 {
+    stdio_init_all();
+
+    WifiConnection wifi(SSID, PASSPHRASE);
+    printf("Wifi Connection Status: %s\n", toString(wifi.status()).data());
+
     DHT sensor(DHTType::DHT22, DHT_DATA_PIN, LED_PIN);
     return 0;
 }
