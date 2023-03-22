46cb598653ac020bbb9777c354b60c1472dcf818
Joe Porembski <jporembski@gmail.com>
2023-03-22 11:48:38 -0400

    Update topic names

--- include/environment-monitor.hpp
+++ include/environment-monitor.hpp
@@ -6,6 +6,10 @@ SPDX-License-Identifier: BSD-3-Clause
 
 #include <string_view>
 
-
-inline constexpr std::string_view HUMIDITY_TOPIC_NAME_FORMAT = "%s/climate/sensor/humidity";
-inline constexpr std::string_view TEMPERATURE_TOPIC_NAME_FORMAT = "%s/climate/sensor/temperate";
+inline constexpr std::string_view PROGRAM_TOPIC_NAME_FORMAT = "%s";
+inline constexpr std::string_view VERSION_TOPIC_NAME_FORMAT = "%s/version";
+inline constexpr std::string_view BUILD_DATE_TOPIC_NAME_FORMAT = "%s/build_date";
+inline constexpr std::string_view START_DATE_TOPIC_NAME_FORMAT = "%s/start_date";
+inline constexpr std::string_view BATTERY_TOPIC_NAME_FORMAT = "%s/battery";
+inline constexpr std::string_view HUMIDITY_TOPIC_NAME_FORMAT = "%s/climate/humidity";
+inline constexpr std::string_view TEMPERATURE_TOPIC_NAME_FORMAT = "%s/climate/temperate";
