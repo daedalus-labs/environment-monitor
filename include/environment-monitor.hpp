14abc9f6456cf04254c4c4e25b9b08dbc236c502
Joe Porembski <jporembski@gmail.com>
2023-03-23 15:45:21 -0400

    Add/rename topics

--- include/environment-monitor.hpp
+++ include/environment-monitor.hpp
@@ -6,10 +6,13 @@ SPDX-License-Identifier: BSD-3-Clause
 
 #include <string_view>
 
-inline constexpr std::string_view PROGRAM_TOPIC_NAME_FORMAT = "%s";
-inline constexpr std::string_view VERSION_TOPIC_NAME_FORMAT = "%s/version";
-inline constexpr std::string_view BUILD_DATE_TOPIC_NAME_FORMAT = "%s/build_date";
-inline constexpr std::string_view START_DATE_TOPIC_NAME_FORMAT = "%s/start_date";
-inline constexpr std::string_view BATTERY_TOPIC_NAME_FORMAT = "%s/battery";
-inline constexpr std::string_view HUMIDITY_TOPIC_NAME_FORMAT = "%s/climate/humidity";
-inline constexpr std::string_view TEMPERATURE_TOPIC_NAME_FORMAT = "%s/climate/temperate";
+
+inline constexpr std::string_view PROGRAM_TOPIC_FORMAT = "%s";
+inline constexpr std::string_view VERSION_TOPIC_FORMAT = "%s/version";
+inline constexpr std::string_view BUILD_DATE_TOPIC_FORMAT = "%s/build_date";
+inline constexpr std::string_view START_DATE_TOPIC_FORMAT = "%s/start_date";
+inline constexpr std::string_view BATTERY_CHARGE_TOPIC_FORMAT = "%s/battery/charge";
+inline constexpr std::string_view BATTERY_CHARGING_TOPIC_FORMAT = "%s/battery/charging";
+inline constexpr std::string_view BOARD_TEMPERATURE_TOPIC_FORMAT = "%s/board/temperate";
+inline constexpr std::string_view HUMIDITY_TOPIC_FORMAT = "%s/climate/humidity";
+inline constexpr std::string_view TEMPERATURE_TOPIC_FORMAT = "%s/climate/temperate";
